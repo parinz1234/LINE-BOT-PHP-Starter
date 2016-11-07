465c265569da181babbb47a56b5f7e3266e2b423
parinz1234 <parinya.ons@gmail.com>
2016-11-07 12:33:32 +0700

    fix verify.php

--- verify.php
+++ verify.php
@@ -2,7 +2,7 @@
 $access_token = 'o2vIj5fWJZSpVBzhwbpZuVSF9J50g1QTInfhDZMVQxCmcdLu8uobeHsoKmlmDNzDV67CFAxA9szYJ9Czn+RulSUnLlo0CVKcOiAkj30K6DwPnGJbhc36nw8Cc6OOz0jvb/L9bvhfks0N94WrcR62mgdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
-$header = array('Authorization: Bearer ' . $access_token);
+$headers = array('Authorization: Bearer ' . $access_token);
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
